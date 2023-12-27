0a48cebb7743101d4e0fb2c449157c25bb297110
Charlie Fish <contact@charlie.fish>
2023-12-26 21:05:40 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# MapToImage
+
+## Description
+
+This is a Node.js package that allows you to convert a map to an image. You can set the dimensions of the image, the zoom level, the center of the map, and the tile servers you wish to use.
+
+## Installation
+
+```bash
+npm install maptoimage
+```
+
+## Usage
+
+```js
+const { mapToImage } = require("maptoimage");
+(async () => {
+	mapToImage({
+		"image": {
+			"dimensions": {
+				"width": 1280,
+				"height": 720
+			}
+		},
+		"map": {
+			"center": {
+				"lat": 39.67321,
+				"lng": -104.95140
+			},
+			"zoom": 5,
+			"layers": [
+				"https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+				// https://mesonet.agron.iastate.edu/GIS/ridge.phtml
+				"https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/ridge::USCOMP-N0Q-0/{z}/{x}/{y}.png",
+			]
+		}
+	});
+})();
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+Please remember that the tile servers you use may have their own licenses.
---
+++ src/index.ts
@@ -0,0 +1,177 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as sharp from "sharp";
+import * as axios from "axios";
+
+export interface MapToImageSettings {
+	/**
+	 * The image settings.
+	 */
+	"image": {
+		/**
+		 * The dimensions of the image.
+		 */
+		"dimensions": {
+			/**
+			 * The width of the image.
+			 */
+			"width": number,
+			/**
+			 * The height of the image.
+			 */
+			"height": number
+		}
+	},
+	/**
+	 * The map settings.
+	 */
+	"map": {
+		/**
+		 * The center of the map.
+		 */
+		"center": {
+			/**
+			 * The latitude of the center of the map.
+			 */
+			"lat": number,
+			/**
+			 * The longitude of the center of the map.
+			 */
+			"lng": number
+		},
+		/**
+		 * The zoom level of the map.
+		 *
+		 * Please ensure that your layers support the zoom level you are using.
+		 *
+		 * @see https://wiki.openstreetmap.org/wiki/Zoom_levels
+		 */
+		"zoom": number,
+		/**
+		 * An array of map layers to use.
+		 *
+		 * The system will replace `{z}`, `{x}`, and `{y}` with the zoom level, x coordinate, and y coordinate respectively.
+		 *
+		 * The layers will be layered on top of each other in the order they are in the array. Meaning the first layer in the array will be on the bottom, and the last layer in the array will be on the top.
+		 *
+		 * @example ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"]
+		 */
+		"layers": string[]
+	}
+}
+
+function downloadTileURL(url: string, x: number, y: number, z: number) {
+	return url.replace("{x}", x.toString()).replace("{y}", y.toString()).replace("{z}", z.toString());
+}
+async function downloadTile(url: string) {
+	console.log("Fetching tile: " + url);
+	const result = await axios.default.get(url, {
+		"responseType": "arraybuffer"
+	});
+	const buffer = Buffer.from(result.data, "binary");
+	return buffer;
+}
+
+function coordinatesToTile(lat: number, lng: number, zoom: number) {
+	const n = Math.pow(2, zoom);
+	const x = (n * ((lng + 180) / 360));
+	const latRad = lat * Math.PI / 180;
+	const y = (n * (1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2);
+
+	return {
+		x,
+		y
+	};
+}
+
+export async function mapToImage(settings: MapToImageSettings) {
+	let image = sharp({
+		"create": {
+			"width": settings.image.dimensions.width,
+			"height": settings.image.dimensions.height,
+			"channels": 4,
+			"background": {
+				"r": 0,
+				"g": 0,
+				"b": 0,
+				"alpha": 0
+			}
+		}
+	});
+
+	const imageCenter = {
+		"x": settings.image.dimensions.width / 2,
+		"y": settings.image.dimensions.height / 2
+	};
+
+	let images: { input: string, left: number, top: number }[] = [];
+
+	for (const layer of settings.map.layers) {
+		const tile = coordinatesToTile(settings.map.center.lat, settings.map.center.lng, settings.map.zoom);
+		const offsetOfCoordinates = {
+			"x": (tile.x - Math.floor(tile.x)) * 256,
+			"y": (tile.y - Math.floor(tile.y)) * 256
+		};
+
+		function createImageObject(x: number, y: number, zoom: number, left: number, top: number) {
+			return {
+				"input": downloadTileURL(layer, Math.floor(x), Math.floor(y), zoom),
+				"left": left,
+				"top": top
+			};
+		}
+
+		const img = createImageObject(tile.x, tile.y, settings.map.zoom, Math.round((imageCenter.x - (256 / 2))) + Math.round((256 / 2) - offsetOfCoordinates.x), Math.round((imageCenter.y - (256 / 2))) + Math.round((256 / 2) - offsetOfCoordinates.y));
+		images.push(img);
+
+		// We now have the primary tile that the user asked for.
+		// We now need to fill in the rest of the image with surrounding tiles.
+		// To do this we create two functions to move in the Y and X directions. The X direction will call the Y direction.
+		// The pattern looks like this:
+		// Right -> Down -> Up
+		// Left -> Down -> Up
+		// So it'll go to the right one tile, then go down all the way, then up all the way. Then it'll continue by going to the right one more tile, then down all the way, then up all the way. And so on until the end of the image.
+		// After that it'll repeat the same thing but for the left side of the image.
+		function moveInDirectionY(tile: { x: number, y: number }, zoom: number, img: any, currentBorder: number, x: number, direction: "down" | "up") {
+			let currentDownBorder: number = img.top;
+			let numB = 0;
+			while (direction === "down" ? currentDownBorder < settings.image.dimensions.height : currentDownBorder > -256) {
+				images.push(createImageObject(x, Math.floor(tile.y) + (direction === "down" ? numB : -numB), settings.map.zoom, currentBorder, currentDownBorder));
+				currentDownBorder += direction === "down" ? 256 : -256;
+				numB++;
+			}
+		}
+		function moveInDirectionX(tile: { x: number, y: number }, zoom: number, img: any, direction: "right" | "left") {
+			let currentRightBorder: number = img.left;
+			let numA = 0;
+			while (direction === "right" ? currentRightBorder < settings.image.dimensions.width : currentRightBorder > -256) {
+				const x = Math.floor(tile.x) + (direction === "right" ? numA : -numA);
+				images.push(createImageObject(x, Math.floor(tile.y), settings.map.zoom, currentRightBorder, img.top));
+
+				// Down
+				moveInDirectionY(tile, settings.map.zoom, img, currentRightBorder, x, "down");
+
+				// Up
+				moveInDirectionY(tile, settings.map.zoom, img, currentRightBorder, x, "up");
+
+				currentRightBorder += direction === "right" ? 256 : -256;
+				numA++;
+			}
+		}
+		moveInDirectionX(tile, settings.map.zoom, img, "right");
+		moveInDirectionX(tile, settings.map.zoom, img, "left");
+	}
+
+	image = image.composite(await Promise.all(images.filter((img, _index, array) => {
+		return array.findIndex((img2) => {
+			return img2.input === img.input;
+		}) === _index;
+	}).map(async (img) => {
+		return {
+			...img,
+			"input": await downloadTile(img.input)
+		}
+	})));
+
+	return image;
+}
